c5bff69e38b38561e2814123d1fd21d50a7431fa
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-05 21:09:08 +0300

    Update startup script to seed roles into roles table

--- src/scripts.py
+++ src/scripts.py
@@ -12,23 +12,28 @@ async def create_initial_roles(session: AsyncSession):
 
     # --- Права ---
     # Право на изменение своих пользовательских данных
-    right_read_wright_own_user = Right(
+    right_read_right_own_user = Right(
         read=True, write=True, only_own=True, model="User"
     )
     # Право на любые действия с пользователями (админское)
     right_full_user = Right(read=True, write=True, only_own=False, model="User")
     # Право на создание и изменение своих объявлений
-    right_read_wright_own_ad = Right(
+    right_read_right_own_ad = Right(
         read=True, write=True, only_own=True, model="Advertisement"
+    )
+    # Право на чтение любых объявлений
+    right_read_any_ad = Right(
+        read=True, write=False, only_own=False, model="Advertisement"
     )
     # Право на любые действия с объявлениями (админское)
     right_full_ad = Right(read=True, write=True, only_own=False, model="Advertisement")
 
     session.add_all(
         [
-            right_read_wright_own_user,
+            right_read_right_own_user,
             right_full_user,
-            right_read_wright_own_ad,
+            right_read_right_own_ad,
+            right_read_any_ad,
             right_full_ad,
         ]
     )
@@ -36,7 +41,11 @@ async def create_initial_roles(session: AsyncSession):
 
     # --- Роли ---
     role_user = Role(name="user")
-    role_user.rights = [right_read_wright_own_user, right_read_wright_own_ad]
+    role_user.rights = [
+        right_read_right_own_user,
+        right_read_right_own_ad,
+        right_read_any_ad,
+    ]
 
     role_admin = Role(name="admin")
     role_admin.rights = [right_full_user, right_full_ad]
